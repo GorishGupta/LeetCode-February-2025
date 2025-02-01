686ff3a6094d5b684c2b260fab600cea09683025
Gorish Gupta <gorishgupta50@gmail.com>
2025-02-01 19:16:53 +0530

    Create DS3151.java

---
+++ DS3151.java
@@ -0,0 +1,15 @@
+public class DS3151{
+    public boolean isArraySpecial(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i] % 2 == nums[i+1] % 2){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        DS3151 obj = new DS3151();
+        int[] nums = {1, 2, 3, 4, 5};
+        System.out.println(obj.isArraySpecial(nums));
+    }
+}
