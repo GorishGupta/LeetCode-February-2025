86e1d04c5c9fff957995670438319fc136bdacb9
Gorish Gupta <gorishgupta50@gmail.com>
2025-02-25 12:03:40 +0530

    Update DS1524.java

---
+++ DS1524.java
@@ -0,0 +1,13 @@
+public class DS1524 {
+    class Solution {
+        public int numOfSubarrays(int[] arr) {
+            long oddCount = 0, prefixSum = 0;
+            for(int a : arr){
+                prefixSum += a;
+                oddCount += prefixSum % 2;
+            }
+            oddCount += (arr.length - oddCount) * oddCount;
+            return (int)(oddCount % 1_000_000_007);
+        }
+    }
+}
