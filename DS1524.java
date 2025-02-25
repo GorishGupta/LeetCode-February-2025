85676c49ff8da9c794479acc8109028f4e2de969
Gorish Gupta <gorishgupta50@gmail.com>
2025-02-25 12:04:07 +0530

    Update DS1524.java

--- DS1524.java
+++ DS1524.java
@@ -11,6 +11,19 @@ public class DS1524 {
         }
     }
     public static void main(String[] args) {
+        DS1524 ds1524 = new DS1524();
+        DS1524.Solution solution = ds1524.new Solution();
         
+        // Test cases
+        int[] arr = {1, 3, 5};
+        System.out.println(solution.numOfSubarrays(arr)); // 4
+        arr = new int[]{2, 4, 6};
+        System.out.println(solution.numOfSubarrays(arr)); // 0
+        arr = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println(solution.numOfSubarrays(arr)); // 16
+        arr = new int[]{100, 100, 99, 99};
+        System.out.println(solution.numOfSubarrays(arr)); // 4
+        arr = new int[]{7};
+        System.out.println(solution.numOfSubarrays(arr)); // 1  
     }
 }
