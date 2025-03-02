1208dce69243c339f76939c31d64bdc223c5fd16
Gorish Gupta <gorishgupta50@gmail.com>
2025-03-02 22:27:12 +0530

    Update DS2570.java

--- DS2570.java
+++ DS2570.java
@@ -35,4 +35,38 @@ public class DS2570 {
         return result.toArray(new int[result.size()][]);
     }
 }
+public static void main(String[] args) {
+    DS2570 ds2570 = new DS2570();
+    DS2570.Solution solution = ds2570.new Solution();
+    
+    // Test cases
+    int[][] nums1 = {{1, 3}, {2, 4}, {5, 6}};
+    int[][] nums2 = {{1, 2}, {3, 5}, {7, 8}};
+    int[][] res = solution.mergeArrays(nums1, nums2);
+    for(int[] num : res){
+        System.out.print("[" + num[0] + ", " + num[1] + "] ");
+    }
+    System.out.println();
+    nums1 = new int[][]{{1, 3}, {2, 4}, {5, 6}};
+    nums2 = new int[][]{{1, 2}, {3, 5}, {7, 8}, {9, 10}};
+    res = solution.mergeArrays(nums1, nums2);
+    for(int[] num : res){
+        System.out.print("[" + num[0] + ", " + num[1] + "] ");
+    }
+    System.out.println();
+    nums1 = new int[][]{{1, 3}, {2, 4}, {5, 6}, {7, 8}};
+    nums2 = new int[][]{{1, 2}, {3, 5}, {7, 8}};
+    res = solution.mergeArrays(nums1, nums2);
+    for(int[] num : res){
+        System.out.print("[" + num[0] + ", " + num[1] + "] ");
+    }
+    System.out.println();
+    nums1 = new int[][]{{1, 3}, {2, 4}, {5, 6}, {7, 8}};
+    nums2 = new int[][]{{1, 2}, {3, 5}, {7, 8}, {9, 10}};
+    res = solution.mergeArrays(nums1, nums2);
+    for(int[] num : res){
+        System.out.print("[" + num[0] + ", " + num[1] + "] ");
+    }
+    System.out.println();
+}
 }
