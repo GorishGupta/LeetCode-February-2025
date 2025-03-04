6d2d591f63b98d08eae3ff32a0a434dff07a7831
Gorish Gupta <gorishgupta50@gmail.com>
2025-03-04 15:28:57 +0530

    Update DS1780.java

--- DS1780.java
+++ DS1780.java
@@ -11,8 +11,7 @@ public class DS1780 {
         }
         public static void main(String[] args) {
            int n = 27;
-              Solution solution = new Solution();
-                System.out.println(solution.checkPowerofThree(n));
+                System.out.println(checkPowerofThree(n));
                 
         }
     }
