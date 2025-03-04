668f8cbb76effce635dcfef8641f4d19cba3aeb9
Gorish Gupta <gorishgupta50@gmail.com>
2025-03-04 15:30:05 +0530

    Update DS1780.java

--- DS1780.java
+++ DS1780.java
@@ -1,4 +1,6 @@
 public class DS1780 {
+    // 1780. Check if Number is a Sum of Powers of Three
+    // Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
     class Solution{
         public boolean checkPowerofThree (int n){
             if(n<=0){
@@ -9,5 +11,10 @@ public class DS1780 {
             }
             return true;
         }
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.checkPowerofThree(12));
+        System.out.println(s.checkPowerofThree(91));
     }
 }
